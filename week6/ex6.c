7b9ab84025c79732d0b15649db3ac15210b41d61
Vyacheslav Shults <shulz134700@gmail.com>
2018-09-26 23:42:32 +0300

    week6

--- week6/ex6.c
+++ week6/ex6.c
@@ -6,35 +6,33 @@
 #include <sys/wait.h>
 
 int main() {
-//    I don't know why this program does not work and how presence of sleep() affect on this because when I added this
-//    program has broken
     int fd[2];
     printf("Create first child\n");
+    pipe(fd);
     int pid1 = fork();
     if (pid1 != 0) {
         printf("Create second child\n");
         int pid2 = fork();
-        if (pid2 != 0) {
+        if (pid2) {
             printf("Record pid2 %d in buffer\n", pid2);
-            write(fd[1], (const void *) pid2, sizeof(pid2));
+            write(fd[1], &pid2, sizeof(pid2));
         } else {
             printf("Magic is here\n");
-            for (int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 1000; ++i) {
                 printf("Magic appears here %d times\n", i);
                 sleep(1);
             }
         }
 
     } else {
-        sleep(1);
         int *status;
-        pid_t *pid2;
+        pid_t pid2 = 0;
 
-        read(fd[0], pid2, sizeof(pid2));
-        printf("Read pid2 %d from buffer\n", *pid2);
-        waitpid(*pid2, status, 0);
+        read(fd[0], &pid2, sizeof(pid2));
+        printf("Read pid2 %d from buffer\n", pid2);
+        waitpid(pid2, status, 0);
         sleep(2);
-        kill(*pid2, 1);
+        kill(pid2, 1);
 
     }
 
